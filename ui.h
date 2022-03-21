797d2fe1e366034ba9d8e65d45c328aaf4bfe219
German <german.fidarov@gmail.com>
2022-03-21 17:45:55 +0300

    initial commit

---
+++ ui.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int getIntChoice(int n, int choices)
+{
+
+}
+
+void showmenu()
+{
+    printf("1. Create new good configuration\n"
+    "2. Save current configuration to file\n"
+    "3. Import configuration from file\n"
+    "4. View/edit current configuration\n"
+    "5. Calculate price per item\n\n"
+    "0. Exit\n"
+    "You can always display menu by typing 'h'!\n");
+}
